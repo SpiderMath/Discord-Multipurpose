08dd87d159e62576bcf353a3bebb64b04abf300d
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-04-17 06:57:08 +0530

    Added .npmignore & made Base

--- Source/Index.ts
+++ Source/Index.ts
@@ -1,1 +1,9 @@
-export default class DiscordMultipurpose {};
+import { version } from "../package.json";
+import Util from "./Classes/Util";
+import MiniGames from "./Classes/Minigames";
+
+export default class DiscordMultipurpose {
+	static version: string = version;
+	static Util = Util;
+	static MiniGames = MiniGames;
+};
