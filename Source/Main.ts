f229d12121f1bf086e1b152bfa73779c86e0043f
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-19 14:39:04 +0600

    ✅Yarn for others and Import as a whole

--- Source/Main.ts
+++ Source/Main.ts
@@ -1,7 +1,11 @@
 import DiscordEmoji from "./Plugins/DiscordEmoji";
 import Image from "./Plugins/Image";
 
+const multipurpose = { DiscordEmoji, Image }
+
 export {
 	Image,
 	DiscordEmoji,
 };
+
+export default multipurpose; //* import multipurpose from 'discord-multipurpose'; (import as a whole)
