841f3579e3a61cfd39dd1a5ef6cbecad4c2eb9cf
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-19 15:50:34 +0600

    😁Exporting everything for shipping

--- Source/Main.ts
+++ Source/Main.ts
@@ -1,11 +1,15 @@
 import DiscordEmoji from "./Plugins/DiscordEmoji";
 import Image from "./Plugins/Image";
+import confirmation from "./Plugins/confirmation"
+import chatBot from './Plugins/chatBot'
 
-const multipurpose = { DiscordEmoji, Image }
+const multipurpose = { DiscordEmoji, Image, confirmation, chatBot }
 
 export {
 	Image,
 	DiscordEmoji,
+	confirmation,
+	chatBot
 };
 
 export default multipurpose; //* import multipurpose from 'discord-multipurpose'; (import as a whole)
