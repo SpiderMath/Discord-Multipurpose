900b324c2e5058e21eb2aadbd76f75c627ae95e5
Hasib Al Rashid <cadethasib3091@gmail.com>
2021-05-19 15:47:54 +0600

    ✅Added Chatbot to it

---
+++ Source/Plugins/chatBot.ts
@@ -0,0 +1,12 @@
+import { Message } from 'discord.js'
+import { default as axios } from 'axios'
+
+async function chatBot(message: Message, input: string, uuid = 0o101) {
+    if (!message)
+        throw new ReferenceError('"message" is not defined');
+    if (!input) throw new ReferenceError('"input" is not defined');
+    axios.get(`https://api.monkedev.com/fun/chat?msg=${encodeURIComponent(input)}&uid=${uuid}`).then((response) => {
+        message.reply(response.data.response)
+    })
+}
+module.exports = chatBot;
