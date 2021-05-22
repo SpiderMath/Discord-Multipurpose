2030137a4c102ad4bc6a42417fba7d8cf45b5c35
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-05-22 23:24:32 +0530

    Fixed Package.json :thinking:

--- Dist/Plugins/chatBot.d.ts
+++
@@ -1,4 +0,0 @@
-import { Message } from 'discord.js';
-declare function chatBot(message: Message, input: string, uuid?: number): Promise<void>;
-export default chatBot;
-//# sourceMappingURL=chatBot.d.ts.map
