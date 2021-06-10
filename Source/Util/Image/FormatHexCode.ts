4c4350e165da88f61cabc1ae2326040459cb6a5f
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-10 11:40:01 +0530

    Fixed WS. Removed FakeReply

--- Source/Util/Image/FormatHexCode.ts
+++
@@ -1,8 +0,0 @@
-export default function FormatHexCode(hex: string, alt = "#000000") {
-	if (!hex || typeof hex !== "string") return alt || "#000000";
-	hex = hex.replace("#", "");
-	if (hex.length === 3) hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-	if (hex.length !== 6) return alt || "#000000";
-
-	return `#${hex}`;
-};
--- Source/Util/Image/Shorten.ts
+++
@@ -1,5 +0,0 @@
-export default function shorten(text: string, len: number) {
-	if (typeof text !== "string") return "";
-	if (text.length <= len) return text;
-	return text.substr(0, len).trim() + "...";
-};
