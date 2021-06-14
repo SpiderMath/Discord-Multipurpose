f430a8cdcc3e7a34db03e4ab3bd274f8f090b64d
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2021-06-14 14:45:44 +0530

    Started Rewrite, since Discord.js v13 is coming!

--- Tests/DiscordEmoji.js
+++
@@ -1,4 +0,0 @@
-const { DiscordEmoji } = require("../Dist/Main");
-
-console.log(DiscordEmoji.food.apple);
-// Successful
--- Tests/Image/Invert.js
+++
@@ -1,7 +0,0 @@
-const { writeFileSync } = require("fs");
-const { Image } = require("../../Dist/Main");
-
-const avatar = "https://cdn.discordapp.com/avatars/839367177899737108/a78a0def00e5c41d86a6708376aa3c7b.png?size=256";
-
-Image.invert(avatar)
-	.then(buffer => writeFileSync("invert.png", buffer));
